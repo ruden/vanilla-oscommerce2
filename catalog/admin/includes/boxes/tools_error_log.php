ccc7e558182d6bee93cba597a3056ff47812ffe7
ruden <valera.ruden@gmail.com>
2020-01-26 14:44:43 +0200

    fix box group code

--- catalog/admin/includes/boxes/tools_error_log.php
+++ catalog/admin/includes/boxes/tools_error_log.php
@@ -12,7 +12,7 @@
 
   foreach ($cl_box_groups as &$group) {
     if ($group['heading'] == BOX_HEADING_TOOLS) {
-      $group['apps'][] = array('code' => 'database_tables.php',
+      $group['apps'][] = array('code' => 'error_log.php',
                                'title' => MODULES_ADMIN_MENU_TOOLS_ERROR_LOG,
                                'link' => tep_href_link('error_log.php'));
 
