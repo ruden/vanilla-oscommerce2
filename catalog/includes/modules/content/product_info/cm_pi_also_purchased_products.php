db358eb8c689875243694bc420ab26b6f0c9a57b
ruden <valera.ruden@gmail.com>
2020-02-15 14:25:08 +0200

    remove caching also purchased products

--- catalog/includes/modules/content/product_info/cm_pi_also_purchased_products.php
+++ catalog/includes/modules/content/product_info/cm_pi_also_purchased_products.php
@@ -11,14 +11,14 @@
 */
 
   class cm_pi_also_purchased_products {
-    var $code;
-    var $group;
-    var $title;
-    var $description;
-    var $sort_order;
-    var $enabled = false;
+    public $code;
+    public $group;
+    public $title;
+    public $description;
+    public $sort_order;
+    public $enabled = false;
 
-    function __construct() {
+    public function __construct() {
       $this->code = get_class($this);
       $this->group = basename(dirname(__FILE__));
 
@@ -31,10 +31,10 @@
       }
     }
 
-    function getData() {
-      global $oscTemplate, $language, $currencies;
+    public function execute() {
+      global $oscTemplate, $languages_id, $currencies;
 
-      $orders_query = tep_db_query("select p.products_id, p.products_image from orders_products opa, orders_products opb, orders o, products p, products_description pd where opa.products_id = '" . (int)$_GET['products_id'] . "' and opa.orders_id = opb.orders_id and opb.products_id != '" . (int)$_GET['products_id'] . "' and opb.products_id = p.products_id and opb.orders_id = o.orders_id and pd.products_id = p.products_id and pd.language_id = '" . (int)$language . "' and p.products_status = '1' group by p.products_id, o.date_purchased order by o.date_purchased desc limit " . MODULE_CONTENT_ALSO_PURCHASED_PRODUCTS_MAX_DISPLAY_PRODUCTS);
+      $orders_query = tep_db_query("select p.*, pd.* from orders_products opa left join orders_products opb on opa.orders_id = opb.orders_id left join orders o on opb.orders_id = o.orders_id left join products p on opb.products_id = p.products_id left join products_description pd on pd.products_id = p.products_id where opa.products_id = '" . (int)$_GET['products_id'] . "' and opb.products_id != '" . (int)$_GET['products_id'] . "' and pd.language_id = '" . (int)$languages_id . "' and p.products_status = '1' group by p.products_id, o.date_purchased order by o.date_purchased desc limit " . MODULE_CONTENT_ALSO_PURCHASED_PRODUCTS_MAX_DISPLAY_PRODUCTS);
       $num_products_ordered = tep_db_num_rows($orders_query);
 
       if ($num_products_ordered >= MODULE_CONTENT_ALSO_PURCHASED_PRODUCTS_MIN_DISPLAY_PRODUCTS) {
@@ -47,58 +47,30 @@
         ob_start();
         include('includes/modules/content/' . $this->group . '/templates/also_purchased_products.php');
 
-        return ob_get_clean();
+        $oscTemplate->addContent(ob_get_clean(), $this->group);
       }
     }
 
-    function execute() {
-      global $SID, $oscTemplate;
-
-      if ((USE_CACHE == 'true') && empty($SID)) {
-        $output = $this->cache(3600);
-      } else {
-        $output = $this->getData();
-      }
-
-      $oscTemplate->addContent($output, $this->group);
-    }
-
-    function isEnabled() {
+    public function isEnabled() {
       return $this->enabled;
     }
 
-    function check() {
+    public function check() {
       return defined('MODULE_CONTENT_ALSO_PURCHASED_PRODUCTS_STATUS');
     }
 
-    function install() {
-      tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) values ('Enable New User Module', 'MODULE_CONTENT_ALSO_PURCHASED_PRODUCTS_STATUS', 'True', 'Do you want to enable the new user module?', '6', '1', 'tep_cfg_select_option(array(\'True\', \'False\'), ', now())");
-      tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Min products', 'MODULE_CONTENT_ALSO_PURCHASED_PRODUCTS_MIN_DISPLAY_PRODUCTS', '1', 'Minimum number of products to display.', '6', '0', now())");
+    public function install() {
+      tep_db_query("INSERT INTO configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, set_function, date_added) VALUES ('Enable New User Module', 'MODULE_CONTENT_ALSO_PURCHASED_PRODUCTS_STATUS', 'True', 'Do you want to enable the new user module?', '6', '1', 'tep_cfg_select_option(array(\'True\', \'False\'), ', now())");
+      tep_db_query("INSERT INTO configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) VALUES ('Min products', 'MODULE_CONTENT_ALSO_PURCHASED_PRODUCTS_MIN_DISPLAY_PRODUCTS', '1', 'Minimum number of products to display.', '6', '0', now())");
       tep_db_query("INSERT INTO configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) VALUES ('Max products', 'MODULE_CONTENT_ALSO_PURCHASED_PRODUCTS_MAX_DISPLAY_PRODUCTS', '6', 'Maximum number of products to display.', '6', '0', now())");
-      tep_db_query("insert into configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) values ('Sort Order', 'MODULE_CONTENT_ALSO_PURCHASED_PRODUCTS_SORT_ORDER', '0', 'Sort order of display. Lowest is displayed first.', '6', '0', now())");
+      tep_db_query("INSERT INTO configuration (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, sort_order, date_added) VALUES ('Sort Order', 'MODULE_CONTENT_ALSO_PURCHASED_PRODUCTS_SORT_ORDER', '0', 'Sort order of display. Lowest is displayed first.', '6', '0', now())");
     }
 
-    function remove() {
+    public function remove() {
       tep_db_query("delete from configuration where configuration_key in ('" . implode("', '", $this->keys()) . "')");
     }
 
-    function keys() {
+    public function keys() {
       return array('MODULE_CONTENT_ALSO_PURCHASED_PRODUCTS_STATUS', 'MODULE_CONTENT_ALSO_PURCHASED_PRODUCTS_MIN_DISPLAY_PRODUCTS', 'MODULE_CONTENT_ALSO_PURCHASED_PRODUCTS_MAX_DISPLAY_PRODUCTS', 'MODULE_CONTENT_ALSO_PURCHASED_PRODUCTS_SORT_ORDER');
-    }
-
-    function cache($auto_expire = false, $refresh = false) {
-      global $language;
-
-      $cache_output = '';
-
-      if (isset($_GET['products_id']) && is_numeric($_GET['products_id'])) {
-        if (($refresh == true) || !read_cache($cache_output, 'also_purchased-' . $language . '.cache' . $_GET['products_id'], $auto_expire)) {
-          $cache_output = $this->getData();
-
-          write_cache($cache_output, 'also_purchased-' . $language . '.cache' . $_GET['products_id']);
-        }
-      }
-
-      return $cache_output;
     }
   }
