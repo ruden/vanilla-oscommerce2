f498cecf45e09ae7b0f68a57f475e1ed788d07f3
ruden <valera.ruden@gmail.com>
2020-08-06 13:39:46 +0300

    fix isset

--- catalog/includes/actions/update_product.php
+++ catalog/includes/actions/update_product.php
@@ -14,7 +14,7 @@ for ($i = 0, $n = sizeof($_POST['products_id']); $i < $n; $i++) {
   if (isset($_POST['cart_delete']) && in_array($_POST['products_id'][$i], (is_array($_POST['cart_delete']) ? $_POST['cart_delete'] : array()))) {
     $_SESSION['cart']->remove($_POST['products_id'][$i]);
   } else {
-    $attributes = (isset($_POST['id']) && $_POST['id'][$_POST['products_id'][$i]]) ? $_POST['id'][$_POST['products_id'][$i]] : '';
+    $attributes = (isset($_POST['id'][$_POST['products_id'][$i]])) ? $_POST['id'][$_POST['products_id'][$i]] : '';
     $_SESSION['cart']->add_cart($_POST['products_id'][$i], $_POST['cart_quantity'][$i], $attributes, false);
   }
 }
