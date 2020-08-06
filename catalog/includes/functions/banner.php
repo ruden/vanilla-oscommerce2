9fcb7817ff3d0fd32fecf719ba4c49cb0be4c3fe
ruden <valera.ruden@gmail.com>
2020-08-06 13:45:15 +0300

    fix mysql 8

--- catalog/includes/functions/banner.php
+++ catalog/includes/functions/banner.php
@@ -25,7 +25,7 @@
 ////
 // Auto activate banners
   function tep_activate_banners() {
-    $banners_query = tep_db_query("select banners_id, date_scheduled from banners where date_scheduled != ''");
+    $banners_query = tep_db_query("select banners_id, date_scheduled from banners where date_scheduled is not null");
     if (tep_db_num_rows($banners_query)) {
       while ($banners = tep_db_fetch_array($banners_query)) {
         if (date('Y-m-d H:i:s') >= $banners['date_scheduled']) {
