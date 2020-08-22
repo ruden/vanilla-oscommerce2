0220fd6ab4ef9cb88ba0742629a9ac21ecbedadf
ruden <valera.ruden@gmail.com>
2020-08-22 15:18:07 +0300

    fix link

--- catalog/includes/modules/header/templates/account.php
+++ catalog/includes/modules/header/templates/account.php
@@ -1,6 +1,6 @@
 <div class="t-account my-auto col-auto">
 
-  <a href="<?php echo tep_href_link('login.php'); ?>" title="<?php echo HEADER_TITLE_LOGIN . ' | ' . HEADER_TITLE_CREATE_ACCOUNT; ?>">
+  <a href="<?php echo tep_href_link('account.php'); ?>" title="<?php echo HEADER_TITLE_LOGIN . ' | ' . HEADER_TITLE_CREATE_ACCOUNT; ?>">
     <svg>
       <use href="#svg-icon-user"/>
     </svg>
