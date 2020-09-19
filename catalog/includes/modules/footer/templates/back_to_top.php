cfb0521be61536c0b6aac2af283dbd09131be406
ruden <valera.ruden@gmail.com>
2020-09-19 10:28:13 +0200

    fix class

--- catalog/includes/modules/footer/templates/back_to_top.php
+++ catalog/includes/modules/footer/templates/back_to_top.php
@@ -1,13 +1,13 @@
-<a>&uarr;</a>
+<a class="back-to-top">&uarr;</a>
 
 <script>
   (function () {
     const style = document.createElement('style')
 
-    style.innerHTML = '.t-back-to-top{position:fixed;bottom:80px;right:40px;z-index:9999;width:2.5rem;height:2.5rem;text-align:center;line-height:2rem;background:var(--bs-secondary);color:var(--bs-white);cursor:pointer;border-radius:2px;display:none}.t-back-to-top:hover{background:var(--bs-light);}'
+    style.innerHTML = '.back-to-top{position:fixed;bottom:80px;right:40px;z-index:9999;width:2.5rem;height:2.5rem;text-align:center;line-height:2rem;background:var(--bs-secondary);color:var(--bs-white);cursor:pointer;border-radius:2px;display:none}.back-to-top:hover{background:var(--bs-light);}'
     document.head.appendChild(style)
 
-    const goTopBtn = document.querySelector('.t-back-to-top')
+    const goTopBtn = document.querySelector('.back-to-top')
 
     function trackScroll() {
       let scrolled = window.pageYOffset;
