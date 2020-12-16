0811d62de3107340401fdc462b99885c190e1b8c
ruden <valera.ruden@gmail.com>
2020-12-16 13:21:18 +0200

    fix a8644e1cbb

--- catalog/includes/template_bottom.php
+++ catalog/includes/template_bottom.php
@@ -24,7 +24,7 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="ext/jquery/jquery.min.js"><\/script>')</script>
 
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
 
 <?php
 if (defined('ACCOUNT_DOB') && ACCOUNT_DOB == 'true' && in_array($PHP_SELF, array('create_account.php', 'account_edit.php'))) {
