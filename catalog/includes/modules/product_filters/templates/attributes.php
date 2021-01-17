c79747595694730377934f51edbdcbce6421ebaa
ruden <valera.ruden@gmail.com>
2021-01-18 01:56:18 +0200

    delete test data

--- catalog/includes/modules/product_filters/templates/attributes.php
+++ catalog/includes/modules/product_filters/templates/attributes.php
@@ -3,7 +3,7 @@ foreach ($products_attributes_array as $products_attributes) {
   ?>
 
   <div class="border-1 border-bottom mb-3">
-    <h6><?php echo $products_attributes['options_name'] . ' ' . $products_attributes['options_id']; ?></h6>
+    <h6><?php echo $products_attributes['options_name']; ?></h6>
 
     <div class="mb-3">
 
@@ -19,7 +19,7 @@ foreach ($products_attributes_array as $products_attributes) {
         <div class="form-check">
           <?php echo tep_draw_checkbox_field('attrib_' . $products_attributes['options_id'] . '[]', $options_value['products_options_values_id'], $selected, 'class="form-check-input" id="po-id-' . $options_value['products_options_values_id'] . '" onchange="this.form.submit()"'); ?>
           <label class="form-check-label" for="po-id-<?php echo $options_value['products_options_values_id']; ?>">
-            <?php echo $options_value['products_options_values_name'] . ' ' . $options_value['products_options_values_id']; ?>
+            <?php echo $options_value['products_options_values_name']; ?>
           </label>
         </div>
 
