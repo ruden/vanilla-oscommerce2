e82600d00175c9e91675d865dee93357874db469
ruden <valera.ruden@gmail.com>
2021-02-04 23:23:35 +0200

    add class

--- catalog/includes/modules/content/product_info/templates/image_lightbox.php
+++ catalog/includes/modules/content/product_info/templates/image_lightbox.php
@@ -2,31 +2,29 @@
 
   <?php
   if (!empty($products_images_array)) {
-  foreach ($products_images_array
+    foreach ($products_images_array as $k => $products) {
+      if ($k == 0) {
+        ?>
 
-           as $k => $products) {
-  if ($k == 0) {
-    ?>
+        <a href="<?php echo tep_href_link('images/products/originals/' . $products['image']); ?>" class="lightbox"><?php echo tep_image('images/products/originals/' . $products['image'], $products_name, (int)MODULE_CONTENT_PRODUCT_INFO_IMAGE_LIGHTBOX_ORIGINAL_IMAGE_WIDTH, (int)MODULE_CONTENT_PRODUCT_INFO_IMAGE_LIGHTBOX_ORIGINAL_IMAGE_HEIGHT, 'class="img-fluid mb-3"'); ?></a>
 
-    <a href="<?php echo tep_href_link('images/products/originals/' . $products['image']); ?>" class="lightbox"><?php echo tep_image('images/products/originals/' . $products['image'], $products_name, (int)MODULE_CONTENT_PRODUCT_INFO_IMAGE_LIGHTBOX_ORIGINAL_IMAGE_WIDTH, (int)MODULE_CONTENT_PRODUCT_INFO_IMAGE_LIGHTBOX_ORIGINAL_IMAGE_HEIGHT); ?></a>
+        <?php
+      } else {
+        if (!empty($products['htmlcontent'])) {
+          ?>
 
-    <?php
-  } else {
-  if (!empty($products['htmlcontent'])) {
-    ?>
+          <a href="<?php echo $products['htmlcontent']; ?>" data-type="iframe" class="lightbox"><?php echo tep_image('images/products/thumbs/' . $products['image'], $products_name, (int)MODULE_CONTENT_PRODUCT_INFO_IMAGE_LIGHTBOX_THUMB_IMAGE_WIDTH, (int)MODULE_CONTENT_PRODUCT_INFO_IMAGE_LIGHTBOX_THUMB_IMAGE_HEIGHT, 'class="img-fluid"'); ?></a>
 
-    <a href="<?php echo $products['htmlcontent']; ?>" data-type="iframe" class="lightbox"><?php echo tep_image('images/products/thumbs/' . $products['image'], $products_name, (int)MODULE_CONTENT_PRODUCT_INFO_IMAGE_LIGHTBOX_THUMB_IMAGE_WIDTH, (int)MODULE_CONTENT_PRODUCT_INFO_IMAGE_LIGHTBOX_THUMB_IMAGE_HEIGHT); ?></a>
+          <?php
+        } else {
+          ?>
 
-    <?php
-  } else {
-    ?>
+          <a href="<?php echo tep_href_link('images/products/originals/' . $products['image']); ?>" class="lightbox"><?php echo tep_image('images/products/thumbs/' . $products['image'], $products_name, (int)MODULE_CONTENT_PRODUCT_INFO_IMAGE_LIGHTBOX_THUMB_IMAGE_WIDTH, (int)MODULE_CONTENT_PRODUCT_INFO_IMAGE_LIGHTBOX_THUMB_IMAGE_HEIGHT, 'class="img-fluid"'); ?></a>
 
-    <a href="<?php echo tep_href_link('images/products/originals/' . $products['image']); ?>" class="lightbox"><?php echo tep_image('images/products/thumbs/' . $products['image'], $products_name, (int)MODULE_CONTENT_PRODUCT_INFO_IMAGE_LIGHTBOX_THUMB_IMAGE_WIDTH, (int)MODULE_CONTENT_PRODUCT_INFO_IMAGE_LIGHTBOX_THUMB_IMAGE_HEIGHT); ?></a>
-
-  <?php
-  }
-  }
-  }
+        <?php
+        }
+      }
+    }
   ?>
 
     <link rel="stylesheet" href="includes/modules/content/product_info/templates/image_lightbox/css/tobi.min.css">
