c98dd834e1eaeba58101f1bbf5f3c57b18401f16
ruden <valera.ruden@gmail.com>
2021-02-15 15:22:31 +0200

    fix tls test

--- catalog/includes/apps/paypal/admin/actions/ssltest.php
+++ catalog/includes/apps/paypal/admin/actions/ssltest.php
@@ -10,23 +10,22 @@
   Released under the GNU General Public License
 */
 
-  $curl_info = curl_version();
+$curl_info = curl_version();
 
-  $result = array(
-    'rpcStatus' => 1,
-    'curl_version' => isset($curl_info['version']) ? $curl_info['version'] : '',
-    'curl_ssl_version' => isset($curl_info['ssl_version']) ? $curl_info['ssl_version'] : ''
-  );
+$result = array(
+  'rpcStatus' => 1,
+  'curl_version' => isset($curl_info['version']) ? $curl_info['version'] : '',
+  'curl_ssl_version' => isset($curl_info['ssl_version']) ? $curl_info['ssl_version'] : ''
+);
 
-  $test = $OSCOM_PayPal->makeApiCall('https://tlstest.paypal.com', null, null, array('returnFull' => true, 'sslVersion' => 0));
+$test = $OSCOM_PayPal->makeApiCall('https://www.howsmyssl.com/a/check', null, null, array('returnFull' => true, 'sslVersion' => 0));
 
-  $result['default'] = (isset($test['info']['http_code']) && ((int)$test['info']['http_code'] === 200));
+$result['default'] = (isset($test['info']['http_code']) && ((int)$test['info']['http_code'] === 200));
 
-  $test = $OSCOM_PayPal->makeApiCall('https://tlstest.paypal.com', null, null, array('returnFull' => true, 'sslVersion' => 6));
+$test = $OSCOM_PayPal->makeApiCall('https://www.howsmyssl.com/a/check', null, null, array('returnFull' => true, 'sslVersion' => 6));
 
-  $result['tlsv12'] = (isset($test['info']['http_code']) && ((int)$test['info']['http_code'] === 200));
+$result['tlsv12'] = (isset($test['info']['http_code']) && ((int)$test['info']['http_code'] === 200));
 
-  echo json_encode($result);
+echo json_encode($result);
 
-  exit;
-?>
+exit;
