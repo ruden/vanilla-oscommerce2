e32576e037ec5a0c9bd3b8de4ef3ff3f01118531
ruden <valera.ruden@gmail.com>
2021-06-24 12:44:25 +0300

    composer update

--- catalog/includes/vendor/composer/autoload_real.php
+++ catalog/includes/vendor/composer/autoload_real.php
@@ -25,7 +25,7 @@ class ComposerAutoloaderInitad66624b1b54eb97cdad447d71120bd0
         require __DIR__ . '/platform_check.php';
 
         spl_autoload_register(array('ComposerAutoloaderInitad66624b1b54eb97cdad447d71120bd0', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInitad66624b1b54eb97cdad447d71120bd0', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
