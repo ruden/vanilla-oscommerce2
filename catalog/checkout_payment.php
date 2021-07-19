8b71930043c5032824a5061530e28382fad797f6
ruden <valera.ruden@gmail.com>
2021-07-20 00:34:20 +0300

    add svg icon for alert

--- catalog/checkout_payment.php
+++ catalog/checkout_payment.php
@@ -142,10 +142,14 @@ require('includes/template_top.php');
     if (isset($_GET['payment_error']) && is_object(${$_GET['payment_error']}) && ($error = ${$_GET['payment_error']}->get_error())) {
       ?>
 
-      <div class="mb-3">
-        <?php echo '<strong>' . tep_output_string_protected($error['title']) . '</strong>'; ?>
-
-        <p class="messageStackError"><?php echo tep_output_string_protected($error['error']); ?></p>
+      <div class="alert alert-danger d-flex align-items-start" role="alert">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
+          <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+        </svg>
+        <div>
+          <strong class="alert-heading"><?php echo tep_output_string_protected($error['title']); ?></strong>
+          <p class="mb-0"><?php echo tep_output_string_protected($error['error']); ?></p>
+        </div>
       </div>
 
       <?php
